312c9734a387e36689f5a499b322e7725bf1ed56
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-09 17:08:17 -0500

    item images appearing as expected

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,72 @@
 
 
 import $ from 'jquery';
+import etsyData from './items';
 
-$('main').hide();
+var items = etsyData.results;
+
+var five = 5;
+
+//////////////define the problem
+
+//////////////1. create the rough html needed for the items
+//////////////2. define what pieces of data you need from the array/object (image, title, creator, price, currency code)
+
+
+///////////Code for the html template
+
+// var itemLocation = $(document.querySelector("main"));
+
+// // var itemLocation = document.querySelector("main");
+
+
+// function itemMarkup(array){
+// 	array.reduce(function(memo,item){
+
+// 	var firstImage = item.Images[0].url_170x135;
+
+// 					return memo +=`<div class="item">
+
+// 					  			<img src="${firstImage}">
+// 					  			<div class="itemTitle"></div>
+// 					  			<div class="makerAndPrice">
+// 					  				<div class="maker"></div>
+// 					  				<div class="price"></div>
+// 					  			</div>
+					  			
+// 					  		</div>`;
+
+// },"")};
+
+// itemLocation.append(itemMarkup(items));
+
+
+var itemLocation = $(document.querySelector("main"));
+
+
+// var itemLocation = document.querySelector("main");
+
+
+function createItem(product){
+
+	var productImg = product.Images[0].url_170x135;
+
+		return `<div class="product">
+
+		  			<img src="${productImg}">
+		  			<div class="productTitle"></div>
+		  			<div class="makerAndPrice">
+		  				<div class="maker"></div>
+		  				<div class="price"></div>
+		  			</div>
+		  			
+		  		</div>`
+
+};
+
+items.forEach(function(product){
+	itemLocation.append(createItem(product));
+})
+
+
+
